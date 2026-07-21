f6f8e83cc33993ac553bf84e9521b8c933be8f96
Vojtěch Deml <235454976+VDeml@users.noreply.github.com>
2026-07-22 01:00:16 +0200

    Add function to check boardgame for win condition

--- script.js
+++ script.js
@@ -37,12 +37,92 @@ function recordMove(playerSymbol, y, x) {
 };
 
 // After every move, we have to check for a win
+function checkForWin() {
+    let win = false;
+    const rows = gameBoard.boardArr.length;
+    const columns = gameBoard.boardArr[0].length;
+    console.log(rows, columns);
+
+    // checks for equality in a column
+    for(let i = 1; i < (rows - 1); i++) {
+        for(let j = 0; j < columns; j++) {
+            if(gameBoard.boardArr[i][j] === gameBoard.boardArr[i-1][j] &&
+            gameBoard.boardArr[i][j] === gameBoard.boardArr[i+1][j]) {
+                if(gameBoard.boardArr[i][j] === undefined)
+                    break;
+                console.log("We found 3 in a column!")
+            };
+        };
+    };
+    // checks for equality in a row
+    for(let i = 0; i < rows; i++) {
+        for(let j = 1; j < (columns -1); j++) {
+            if(gameBoard.boardArr[i][j] === gameBoard.boardArr[i][j-1] &&
+            gameBoard.boardArr[i][j] === gameBoard.boardArr[i][j+1]) {
+                if(gameBoard.boardArr[i][j] === undefined)
+                    break;
+                console.log("We found 3 in a row!")
+            };
+        };
+    };
+    //checks for diagonal equality
+    for(let i = 1; i < (rows - 1); i++) {
+        for(let j = 1; j < (columns -1); j++) {
+            if(gameBoard.boardArr[i][j] === gameBoard.boardArr[i-1][j-1] &&
+            gameBoard.boardArr[i][j] === gameBoard.boardArr[i+1][j+1]) {
+                if(gameBoard.boardArr[i][j] === undefined)
+                    break;
+                console.log("We found 3 diagonally")
+            };
+            if(gameBoard.boardArr[i][j] === gameBoard.boardArr[i-1][j+1] &&
+            gameBoard.boardArr[i][j] === gameBoard.boardArr[i+1][j-1]) {
+                if(gameBoard.boardArr[i][j] === undefined)
+                    break;
+                console.log("We found 3 diagonally")
+            };
+        };
+    };
+};
 
 
+//2nd crack at the checkForWin function
+/*
+function checkForWin() {
+    const x = gameBoard.boardArr[0].length;
+    const y = gameBoard.boardArr.length;
+    let controlArr = []
+
+}
+
+function isThisAWin(array) {
+    let result = array.every(isSame);
+    // function to check is every item of array the same?
+    function isSame(el, index, arr) {
+        if(index === 0) {
+            return true;
+        }
+        else {
+            return el === arr[index -1]
+        }
+    };
+    if(result === true) {
+        console.log("Yea this array is the same through and through")
+    }
+}
+
+*/
+
+//This is to test various board layouts and functions that interact with it
 recordMove("X", 0, 0);
-recordMove("O", 1, 0);
-recordMove("X", 2, 2);
-recordMove("E", 2, 2);
+recordMove("P", 0, 1);
+recordMove("X", 0, 2);
 
+recordMove("X", 1, 0);
+recordMove("R", 1, 1);
+recordMove("E", 1, 2);
+
+recordMove(undefined, 2, 0);
+recordMove(undefined, 2, 1);
+recordMove(undefined, 2, 2);
 console.log(gameBoard.boardArr);
-console.log(typeof(gameBoard.boardArr[0][2]));
+checkForWin();
